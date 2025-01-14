30d834c95172ea15f9639433e407707f94fa16f4
Kristjan Lääne <xonproject@gmail.com>
2025-01-14 19:07:20 +0200

    Shipping Address Form

--- app/(root)/shipping-address/page.tsx
+++ app/(root)/shipping-address/page.tsx
@@ -4,6 +4,7 @@ import { getUserById } from '@/lib/actions/user.actions';
 import { Metadata } from 'next';
 import { redirect } from 'next/navigation';
 import { ShippingAddress } from '@/types';
+import ShippingAddressFrom from './shipping-address-form';
 
 export const metadata: Metadata = {
   title: 'Shipping Address',
@@ -22,7 +23,11 @@ const ShippingAddressPage = async () => {
 
   const user = await getUserById(userId);
 
-  return <>Address</>;
+  return (
+    <>
+      <ShippingAddressFrom address={user.address as ShippingAddress} />
+    </>
+  );
 };
 
 export default ShippingAddressPage;
